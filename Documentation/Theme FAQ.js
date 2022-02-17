b6defb8c9dc1452626dc674a31da11bc681b3449
Raz <78644097+Ridiculous-Workz@users.noreply.github.com>
2022-02-17 21:02:22 +0200

    Theme faq

---
+++ Documentation/Theme FAQ.js
@@ -0,0 +1,4 @@
+/* 
+If you've ever wondered why we have such a complicated way of making themes, its because the way we add themes is unremovable therefore we wont have to 
+waste time updating it.
+*/
