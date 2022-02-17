e367e36dd5fa1c00bbba8f003421cf06bd90d38c
Raz <78644097+Ridiculous-Workz@users.noreply.github.com>
2022-02-17 21:13:03 +0200

    Added Video Demo

--- Documentation/Injection.js
+++ Documentation/Injection.js
@@ -8,3 +8,7 @@ You have two options:
 2. You can turn it back *on* using by setting: "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true 
    in "%appdata%/discord/settings.json".
 */
+
+/*
+Video Explanation Here: https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Art/Toolkit_Demo.mp4
+*/
