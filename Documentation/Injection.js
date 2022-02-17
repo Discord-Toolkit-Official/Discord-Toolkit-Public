fcf8a2e27aa3746e0a5e4ed85fd54e759e4058a3
Raz <78644097+Ridiculous-Workz@users.noreply.github.com>
2022-02-17 20:43:08 +0200

    Added Injection Docs

---
+++ Documentation/Injection.js
@@ -0,0 +1,10 @@
+/* Copy everything from: https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Toolkit.js
+and paste it in console,
+
+if console wont open on stable or ptb use this fix:
+
+You have two options:
+1. You can still open Devtools in the browser.
+2. You can turn it back *on* using by setting: "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true 
+   in "%appdata%/discord/settings.json".
+*/
