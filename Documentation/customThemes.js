18f8b9a08879532e1a8ae740e4ea4c75f8338046
Raz <78644097+Ridiculous-Workz@users.noreply.github.com>
2022-02-18 15:04:46 +0200

    One Line Mode

--- Documentation/customThemes.js
+++ Documentation/customThemes.js
@@ -13,7 +13,13 @@ uninjecttheme("Testid")
 /* Author: {Your Username} */
 function customtheme () {
 injecttheme("CTH", ".tag { css: css }") 
-injecttheme("CTH", ".tag { css: css }") 
+injecttheme("CTH", ".tag2 { css: css }") 
+}
+
+/* or */
+
+function customtheme () {
+injecttheme("CTH", ".tag { css: css } .tag2 { css: css }") 
 }
 
 //E.g Eject (We've done this for you don't worry)
@@ -23,3 +29,5 @@ uninjecttheme("CTH")
 }
 
 //We have added this code into the toolkit you just need to edit it.
+
+//-------------------------------------------------------------------
