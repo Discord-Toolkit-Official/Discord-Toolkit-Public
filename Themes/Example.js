27c6682c1377de53087b6d4a13fcd5861bd88602
Raz <78644097+TRH-Raz@users.noreply.github.com>
2022-02-21 17:29:02 +0200

    added liners

--- Themes/Example.js
+++ Themes/Example.js
@@ -4,7 +4,8 @@ var themeDesc = "Your Description"
 /* Author: Your Discord Username */
 
 //(Don't Edit "CTH")
-//E.g - https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Themes/Author:%20Raz/ExampleTheme.js
+//E.g - Multiple Liner: https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Themes/Author:%20Raz/ExampleTheme.js
+//E.g - One Liner: https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Themes/Author:%20Raz/DarkerThanDarkTheme.js
 function customtheme () {
 injecttheme("CTH", ".tag { css: css }") 
 injecttheme("CTH", ".tag { css: css }") 
