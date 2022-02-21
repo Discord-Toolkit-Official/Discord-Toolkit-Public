35a82a6b8fedbbb50c4796906223a83b585e3ae4
Raz <78644097+TRH-Raz@users.noreply.github.com>
2022-02-21 17:23:03 +0200

    Added Example

---
+++ Themes/Example.js
@@ -0,0 +1,16 @@
+// Theme Info
+var themeName = "Your Theme"
+var themeDesc = "Your Description"
+/* Author: Your Discord Username */
+
+//(Don't Edit "CTH")
+//E.g - https://github.com/Discord-Toolkit-Official/Discord-Toolkit-Public/blob/main/Themes/Author:%20Raz/ExampleTheme.js
+function customtheme () {
+injecttheme("CTH", ".tag { css: css }") 
+injecttheme("CTH", ".tag { css: css }") 
+}
+
+//ejectCustomTheme
+function ejectcustheme () {
+uninjecttheme("CTH") 
+}
