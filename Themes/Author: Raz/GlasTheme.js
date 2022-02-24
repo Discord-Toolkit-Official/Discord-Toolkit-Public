67e7c279d222a5225a7f59b1cfde28b63aa6f3cc
Raz <78644097+TRH-Raz@users.noreply.github.com>
2022-02-24 18:28:22 +0200

    Update and rename SportsTheme.js to GlasTheme.js

--- Themes/Author: Raz/GlasTheme.js
+++ Themes/Author: Raz/GlasTheme.js
@@ -2,7 +2,7 @@ var themeName = "Wyvern Theme"
 var themeDesc = "Discord but green"
 /* Author: Raz#7970 */
 function customtheme () {
-injecttheme("CTH", `.content-1jQy2l, .members-3WRCEx, .thin-31rlnD, .scroller-1ox3I2, .container-ZMc96U.themed-Hp1KC_, .privateChannels-oVe7HL, .scroller-WSmht3, .scroller-3X7KbA, .withFrame-2dL45i, .sidebar-1tnWFu, .theme-dark {
+injecttheme("CTH", `.content-1jQy2l, .theme-dark .root-g14mjS, .peopleColumn-1wMU14, .members-3WRCEx, .thin-31rlnD, .contentRegion-3HkfJJ, .scroller-1ox3I2, .container-ZMc96U.themed-Hp1KC_, .privateChannels-oVe7HL, .scroller-WSmht3, .scroller-3X7KbA, .withFrame-2dL45i, .sidebar-1tnWFu, .theme-dark {
   background-image: url('https://support.discord.com/hc/user_images/taI1y91jaKAlB8vX0_5kag.jpeg');
   background-shading: 100%; /* app background shading (0 for complete smoothness) [default: 100%] */
 	background-overlay: rgba(0, 0, 0, 0.6); /* app background overlay color/gradient [default: rgba(0, 0, 0, 0.6)] */
@@ -16,14 +16,19 @@ injecttheme("CTH", `.content-1jQy2l, .members-3WRCEx, .thin-31rlnD, .scroller-1o
 	background-invert: 0%; /* app background invert (0 - 100%)  [default: 0%] */
 	background-grayscale: 0%; /* app background grayscale ( 0 - 100%) [default: 0%] */
 	background-sepia: 0%; /* app background sepia (0 - 100%) [default: 0%] */
-	background-blur: 0px 
+	background-blur: 0px;
  	--background-primary: #36393f00;
  	--background-secondary: #2f313642;
  	--background-secondary-alt: #292b2f57;
  	--background-tertiary: #2022254a;
  	--background-accent: #4f545c38;
  	--background-floating: #18191c82
-	--scrollbar-auto-track: transparent;
+	--scrollbar-thin-thumb: #20222500;
+    	--scrollbar-thin-track: #761c1c00;
+    	--scrollbar-auto-thumb: #20222500;
+    	--scrollbar-auto-track: #2230;
+    	--scrollbar-auto-scrollbar-color-thumb: #20222500;
+    	--scrollbar-auto-scrollbar-color-track: #2f313600;
 }
 .footer-3naVBw {
     background-color: #18191c00;
