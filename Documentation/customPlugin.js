20ca2ff677d934f13408e22351d46123cf539324
Raz <91196395+Raz-js@users.noreply.github.com>
2022-03-01 13:49:42 +0200

    Added customPlugin

---
+++ Documentation/customPlugin.js
@@ -0,0 +1,17 @@
+// Plugin Info
+var pluginOneName = "Your Plugin"
+var pluginOneDesc = "Your Description"
+/* Author: Your Discord Username */
+
+//Add Your Plugin Code At "//Your Code"
+function customplugin () {
+//Your Code
+}
+
+/* Add Your Plugin Removal Code At "//Your Code"
+- ejectCustomPlugin */
+function ejectcusplugin () {
+//Your Code
+}
+
+//Self-Explanatory
