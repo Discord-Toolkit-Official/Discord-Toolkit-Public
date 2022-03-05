635b9a2bff69389ce1d2eeba98f2d9264b9a1b9a
Raz <91196395+Raz-js@users.noreply.github.com>
2022-03-05 18:26:45 +0200

    Create ProperDarkTheme.js

---
+++ Custom CSS/ProperDarkTheme.js
@@ -0,0 +1,5 @@
+injecttheme("White2Dark", ".container-1Lk8p7 { background: #1c1c1c } .theme-light .footer-31IekZ { background-color: #f6f6f700; box-shadow: inset 0 1px 0 rgb(246 246 247 / 0%) } .container-x8Y1ix { background-color: #160e0e }")
+
+/*
+Turns Some Base Modals Into DarkMode Instead Of Default WhiteMode
+*/
