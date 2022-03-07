d599a5ddc931dd26090431409aa2ab7b3fb9e07f
Raz <91196395+Raz-js@users.noreply.github.com>
2022-03-07 21:47:23 +0200

    Extended The Docs

--- Documentation/customTheme.js
+++ Documentation/customTheme.js
@@ -1,19 +1,23 @@
 /*
-//E.g Inject
+//--------------------------------E.g Inject
 injecttheme("TestId", ".dark-theme { display: none }") 
 --- or --- 
 injecttheme("TestId", `.dark-theme { 
 display: none 
 }`) 
 
-//uninject
+//--------------------------------uninject
 uninjecttheme("TestId") 
 uninjecttheme("Testid") 
 */
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+//--------------------------------Theme Info (Give your theme a name and description)
+var themeName = "Your Theme"
+var themeDesc = "Your Description"
+/* Author: Your Discord Username */
 
-//E.g Inject (Don't Edit "CTH")
-//If you decide to share your theme, make your mark on the theme by adding your username in the "Author" line
-/* Author: {Your Username} */
+//---------------------------------E.g Inject (Don't Edit "CTH")
+//---------------------------------If you decide to share your theme, make your mark on the theme by adding your username in the "Author" line/
 function customtheme () {
 injecttheme("CTH", ".tag { css: css } .tag2 { css: css }")
 }
@@ -30,11 +34,9 @@ css: css
 }`) 
 }
 
-//E.g Eject (We've done this for you don't worry)
+//--------------------------------E.g Eject (We've done this for you don't worry)
 function ejectcustheme () {
 uninjecttheme("CTH") 
 }
 
-//We have added this code into the toolkit you just need to edit it.
-
-//-------------------------------------------------------------------
+/*--------------------------------We have added this code into the toolkit you just need to edit it--------------------------------*/
