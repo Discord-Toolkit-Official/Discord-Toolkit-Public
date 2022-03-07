6720e4ddf9dad498a62038248a4cdee2388bd151
Raz <91196395+Raz-js@users.noreply.github.com>
2022-03-07 22:27:46 +0200

    This is a base to help theme makers!!!!

---
+++ Themes/ImageBase.theme.js
@@ -0,0 +1,50 @@
+var themeName = "Glasscord Theme"
+var themeDesc = "Discord But 100% transparent and you can add pictures"
+/* Author: Raz#7970 */
+function customtheme () {
+injecttheme("CTH", `.content-1jQy2l, .theme-dark .root-g14mjS, .peopleColumn-1wMU14, .members-3WRCEx, .thin-31rlnD, .contentRegion-3HkfJJ, .scroller-1ox3I2, .container-ZMc96U.themed-Hp1KC_, .privateChannels-oVe7HL, .scroller-WSmht3, .scroller-3X7KbA, .withFrame-2dL45i, .sidebar-1tnWFu, .theme-dark {
+  background-image: url('https://support.discord.com/hc/user_images/xlbQOO5MQcqPG3s2tlIukw.jpeg');
+  background-shading: 100%; /* app background shading (0 for complete smoothness) [default: 100%] */
+	background-overlay: rgba(0, 0, 0, 0.6); /* app background overlay color/gradient [default: rgba(0, 0, 0, 0.6)] */
+	background-position: center; /* app background position [default: center] */
+	background-size: cover; /* app background size [default: cover] */
+	background-repeat: no-repeat; /* app background repeat [default: no-repeat] */
+	background-attachment: fixed; /* app background attachment [default: fixed] */
+	background-brightness: 100%; /* app background brightness (< 100% for darken, > 100% for lighten) [default: 100%] */
+	background-contrast: 100%; /* app background contrast [default: 100%] */
+	background-saturation: 100%; /* app background saturation [default: 100%] */
+	background-invert: 0%; /* app background invert (0 - 100%)  [default: 0%] */
+	background-grayscale: 0%; /* app background grayscale ( 0 - 100%) [default: 0%] */
+	background-sepia: 0%; /* app background sepia (0 - 100%) [default: 0%] */
+	background-blur: 30px;
+ 	--background-primary: #36393f00;
+ 	--background-secondary: #2f313642;
+ 	--background-secondary-alt: #292b2f57;
+ 	--background-tertiary: #2022254a;
+ 	--background-accent: #4f545c38;
+ 	--background-floating: #18191c82
+	--scrollbar-thin-thumb: #20222500;
+    	--scrollbar-thin-track: #761c1c00;
+    	--scrollbar-auto-thumb: #20222500;
+    	--scrollbar-auto-track: #2230;
+    	--scrollbar-auto-scrollbar-color-thumb: #20222500;
+    	--scrollbar-auto-scrollbar-color-track: #2f313600;
+}
+.footer-3naVBw {
+    background-color: #18191c00;
+    color: #fff0;
+}
+.bodyInnerWrapper-2bQs1k {
+    border-radius: 10px;
+}
+.scrollableContainer-15eg7h {
+    background-color: #40444b29;
+}
+.channelTextArea-1FufC0 {
+    background-color: #36393f29;
+}
+.platform-win .sidebar-1tnWFu {
+    border-radius: 0px 0 0;
+}
+`)
+}
